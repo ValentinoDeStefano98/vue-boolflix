a65e4a03cd76278b2969e84bd9d90ae37184c933
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-04-26 16:15:14 +0200

    1-5 score added

--- src/components/partials/FilmsCard.vue
+++ src/components/partials/FilmsCard.vue
@@ -1,20 +1,17 @@
 <template>
-  <div>
-      <div>
-          <h5>
-              Titolo: {{arrayFilms.title}}
-          </h5>
-          <h6>
-              Titolo originale: {{arrayFilms.original_title}}
-          </h6>
-          <h6>
-              Lingua:
+  <div class="col py-3">
+      <div class="border bg-dark text-white p-3">
+          <!-- Titolo -->
+          <h4>{{arrayFilms.title}}</h4>
+          <!-- Titolo originale -->
+          <h6>{{arrayFilms.original_title}}</h6>
+          <!-- Lingua del contenuto -->
+          <div class="d-flex justify-content-center align-items-center">
+              <span>Lingua:</span>
               <span class="flagContainer" :class="(arrayFilms.original_language == 'en') ? 'uk' : (arrayFilms.original_language == 'it' ) ? 'ita' : 'unknown' "></span>
-              <!-- {{arrayFilms.original_language}} -->
-          </h6>
-          <h6>
-              Voto: {{arrayFilms.vote_average}}
-          </h6>
+          </div>
+          <!-- Voto del contenuto -->
+          <span>Voto: {{changeScore()}} </span>
       </div>
   </div>
 </template>
@@ -25,6 +22,12 @@ export default {
   name: 'FilmsCard',
   props: {
       arrayFilms: Object
+  },
+  methods: {
+      changeScore() {
+          let score = parseInt(this.arrayFilms.vote_average);
+          return parseInt(score / 2);
+      }
   }
 }
 </script>
--- src/components/partials/SeriesCard.vue
+++ src/components/partials/SeriesCard.vue
@@ -1,20 +1,17 @@
 <template>
-  <div>
-       <div>
-          <h5>
-              Titolo: {{arraySeries.name}}
-          </h5>
-          <h6>
-              Titolo originale: {{arraySeries.original_name}}
-          </h6>
-          <h6>
-              Lingua:
+    <div class="col py-3">
+      <div class="border bg-dark text-white p-3">
+          <!-- Titolo -->
+          <h4>{{arraySeries.name}}</h4>
+          <!-- Titolo originale -->
+          <h6>{{arraySeries.original_name}}</h6>
+          <!-- Lingua del contenuto -->
+          <div class="d-flex justify-content-center align-items-center">
+              <span>Lingua:</span>
               <span class="flagContainer" :class="(arraySeries.original_language == 'en') ? 'uk' : (arraySeries.original_language == 'it' ) ? 'ita' : 'unknown' "></span>
-              <!-- {{arraySeries.original_language}} -->
-          </h6>
-          <h6>
-              Voto: {{arraySeries.vote_average}}
-          </h6>
+          </div>
+          <!-- Voto del contenuto -->
+          <span>Voto: {{changeScore()}} </span>
       </div>
   </div>
 </template>
@@ -25,6 +22,12 @@ export default {
   name: 'SeriesCard',
   props: {
       arraySeries: Object
+  },
+  methods: {
+      changeScore() {
+          let score = parseInt(this.arraySeries.vote_average);
+          return parseInt(score / 2);
+      }
   }
 }
 </script>
